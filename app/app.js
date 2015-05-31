f4b029bfb013f54f335abcb830e5f771d53ec733
herereadthis <herereadthis@gmail.com>
2015-05-30 22:54:23 -0400

    sets up react with es6

--- app/app.js
+++ app/app.js
@@ -1,1 +1,9 @@
-module.exports = {};
+var React = require('react');
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <h1>Hello world!</h1>
+        );
+    }
+}
--- app/main.js
+++ app/main.js
@@ -1,8 +1,16 @@
-var app = require('./app.js');
+import React from 'react';
+import App from './app';
 
+// simple test to ensure babel is working
 let foo = 'foo',
     bar = 'bar';
 
 var obj = {foo, bar};
 
 window.console.log('load main JS file with es6', obj);
+// end simple test
+
+React.render(
+    <App />,
+    document.body
+);
