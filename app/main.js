3a00f7ae59fbd7dbb5f1b9c1b7b85a0fb15b7d36
herereadthis <herereadthis@gmail.com>
2015-06-05 00:32:27 -0400

    oh look react route finally funcitoning

--- app/main.js
+++ app/main.js
@@ -3,7 +3,12 @@ import React from 'react';
 
 import Router from 'react-router';
 
-var {DefaultRoute, Route, Link} = Router;
+
+
+require("styles/global.less");
+require("static?!./favicon.ico?output=favicon.ico");
+
+var {DefaultRoute, Route, Link, RouteHandler} = Router;
 
 // simple test to ensure babel is working
 let foo = 'foo',
@@ -15,37 +20,13 @@ window.console.log('load main JS file with es6', obj);
 // end simple test
 
 
-class Dashboard extends React.Component {
+class Code extends React.Component {
     render() {
         return (
             <div>
-                <h1>Foo Foo</h1>
-            </div>
-        );
-    }
-}
-
-
-class Inbox extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Inbox</h1>
-            </div>
-        );
-    }
-}
-
-
-
-class Homepage extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Homepage</h1>
+                <h1>Code</h1>
                 <ul>
-                    <li><Link to="app">Dashboard</Link></li>
-                    <li><Link to="inbox">Inbox</Link></li>
+                    <li><Link to="app">Home</Link></li>
                 </ul>
             </div>
         );
@@ -53,41 +34,30 @@ class Homepage extends React.Component {
 }
 
 
-class App extends React.Component {
-    render() {
+var App = React.createClass({
+    render () {
         return (
             <div>
-                <h1>Hello world, it is nice to meet you!</h1>
+                <h1>App</h1>
                 <ul>
-                    <li><Link to="app">Dashboard</Link></li>
-                    <li><Link to="inbox">Inbox</Link></li>
+                    <li><Link to="code">Code</Link></li>
                 </ul>
+                <RouteHandler />
             </div>
-        );
+        )
     }
-}
+});
 
 
-let routes = (
-    <Route name="app" path="/" handler={Homepage}>
-        <Route name="inbox" path="/inbox" handler={Inbox}/>
+
+var routes = (
+    <Route>
+        <Route name="code" path="code" handler={Code}/>
+        <Route name="app" path="/" handler={App} />
         <DefaultRoute handler={App}/>
     </Route>
 );
 
-
 Router.run(routes, (Handler) => {
-    React.render(
-        <Handler/>,
-        document.body
-    );
+    React.render(<Handler/>, document.body);
 });
-
-
-/*
- React.render(
- <App />,
- document.body
- );
-
- */
