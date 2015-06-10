b55bc95c6491ba7ee3698a41efdca028861c8daf
herereadthis <herereadthis@gmail.com>
2015-06-10 00:06:14 -0400

    more cleanup

--- app/views/Homepage/components/Banner.js
+++ app/views/Homepage/components/Banner.js
@@ -4,37 +4,41 @@ export default class Banner extends React.Component {
     render() {
         return (
             <header role="banner" className="starfield">
-                <div class="bellmaker_container">
+                <div className="bellmaker_container">
                     <div id="construction" role="presentation"></div>
                     <div id="header_panel">
                         <a href=""><span>Stand by for a 90s image!</span></a>
                     </div>
-                    <div class="popup_box_simulator">
-                        <span class="retro_spriter">Welcome to my site!</span>
+                    <div className="popup_box_simulator">
+                        <span className="retro_spriter">Welcome to my site!</span>
 
                         <h1>Here, Read This</h1>
 
-                        <p typeof="foaf:Person" resource="#/me/">Hi! My Name is
+                        <p typeof="foaf:Person" resource="#/me/">
+                            <span>Hi! My Name is </span>
                             <span property="foaf:name">
                                 <span property="foaf:nick">Jimmy</span>
+                                <span> </span>
                                 <span property="foaf:family_name">Ha</span>
                             </span>
                             <span property="foaf:givenname"
                                   content="James"></span>
                             <span property="foaf:logo"
                                   content="http://herereadthis.com/build/images/branding/herereadthis_logo.svg"></span>
-                            and I
+                            <span> and I </span>
                             <span property="v:title" content="UX Web Developer">make web pages</span>,
                             <span resource="http://herereadthis.com/"
                                   property="v:url foaf:homepage"></span>
-                            specializing in user interface development and
-                            Django environments. I live and play in the
+                            <span> specializing in user interface development
+                                and single-page applications. I live and play in
+                                the </span>
                             <span property="v:Postal" resource="#/me/address/"
                                   typeof="v:Address">
                                 <span property="v:locality"
                                       content="Washington"></span>
                                 <span property="v:region">DC</span>
-                            </span> metro area.
+                            </span>
+                            <span> metro area. </span>
                             <span property="foaf:based_near"
                                   resource="#/me/geo/"
                                   typeof="geo:point"
@@ -44,18 +48,21 @@ export default class Banner extends React.Component {
                                 <span property="geo:long"
                                       content="-77.041626"></span>
                             </span>
-                            I do
+                            <span> I do </span>
                             <span
                                 property="foaf:topic_interest">photography</span>
-                            with a Hasselblad camera,
+                            <span> with a Hasselblad camera, </span>
                             <span property="foaf:topic_interest"
-                                  content="gardening">grow roses</span>,
+                                  content="gardening">grow roses</span>
+                            <span>, </span>
                             <span property="foaf:topic_interest"
-                                  content="baking">bake pastries</span>,
-                            dance at <span property="foaf:topic_interest"
-                                           content="Burning Man">Burner festivals</span>,
-                            and practice <span property="foaf:topic_interest">Kyudo</span>.
-                            I put Sriracha on everything.</p>
+                                  content="baking">bake pastries</span>
+                            <span>, dance at </span>
+                            <span property="foaf:topic_interest"
+                                           content="Burning Man">Burner festivals</span>
+                            <span>, and practice </span>
+                            <span property="foaf:topic_interest">Kyudo</span>
+                            <span>. I put Sriracha on everything.</span></p>
                     </div>
                 </div>
             </header>
